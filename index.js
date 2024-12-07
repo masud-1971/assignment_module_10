9ecf137665660e3a653b80dbb6b72a4e1018165d
masud-1971 <msd.rana.2008@gmail.com>
2024-12-08 01:00:33 +0600

    js files

---
+++ index.js
@@ -0,0 +1,45 @@
+// Write a function named calculateDifference that takes two arguments and returns the difference between the first and second arguments.
+
+
+// First variable 
+let a = 60; 
+
+// Second variable 
+let b = 20; 
+
+// Funciton to subtract two variables 
+function calculateDifference(a, b) { 
+	return a - b; 
+} 
+
+// For printing the result 
+console.log("after subtraction : "
+	+ calculateDifference(a, b));
+
+
+// Write a function named isOdd that takes one argument and returns true if the number is odd, and false if it is not.
+
+let num = 18;
+isOdd()
+function isOdd(){
+    if(num % 2 == 0){
+       console.log(num, "Is Not Odd");
+    } else{
+        console.log(num, "Is Odd");
+
+    }
+    
+}
+
+
+
+// Write a function named findMin that takes an array of numbers and returns the smallest number from the array.
+
+
+// var arr = [14, 58, 20, 77, 66, 82, 42, 67, 42, 4]
+// var min = Number.POSITIVE_INFINITY
+// var i, l;
+// for (i=0, l=arr.length; i<l, i++) {
+//   min = Math.min(min, arr[i])
+// }
+// console.log(min)
